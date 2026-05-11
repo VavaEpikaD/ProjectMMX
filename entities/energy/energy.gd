8af7c17da6420ea295676a87a4f0728bd57acfec
BiganRadu <radubigan5@gmail.com>
2026-05-11 06:33:20 +0300

    Add expire time for pickups

--- entities/energy/energy.gd
+++ entities/energy/energy.gd
@@ -1,6 +1,16 @@
 extends Area2D
 
 @export var energy_amount: int = 1
+@export var lifetime: float = 7.0
+
+var _time_alive: float = 0.0
+
+func _process(delta: float) -> void:
+	if lifetime <= 0.0:
+		return
+	_time_alive += delta
+	if _time_alive >= lifetime:
+		queue_free()
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player") and body.has_method("add_energy"):
