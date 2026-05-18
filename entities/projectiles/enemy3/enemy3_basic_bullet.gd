173b0dd557ec4830b3440f77079fa3351c5e7f31
VavaEpikaD <vlad.raul.vasile@gmail.com>
2026-05-18 16:02:52 +0300

    added complex enemy (2 states)

--- entities/projectiles/enemy3/enemy3_basic_bullet.gd
+++ entities/projectiles/enemy3/enemy3_basic_bullet.gd
@@ -1,11 +1,11 @@
 extends Area2D
-class_name Enemy1Projectile
+class_name Enemy3BasicBullet
 
-@export var speed: float = 300.0
+@export var speed: float = 450.0
 @export var lifetime: float = 5.0
 @export var damage: int = 1
 
-var direction: Vector2 = Vector2.RIGHT
+var direction: Vector2 = Vector2.DOWN
 var _time_alive: float = 0.0
 
 func _ready() -> void:
@@ -14,7 +14,7 @@ func _ready() -> void:
 func launch(dir: Vector2) -> void:
 	direction = dir.normalized()
 	if direction == Vector2.ZERO:
-		direction = Vector2.RIGHT
+		direction = Vector2.DOWN
 	rotation = 0.0
 
 func _physics_process(delta: float) -> void:
@@ -27,10 +27,10 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
 func _on_body_entered(body: Node) -> void:
-	if not body.is_in_group("player"):
-		if body is TileMapLayer or body is TileMap or body is StaticBody2D:
-			queue_free()
+	if body.is_in_group("player"):
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
+		queue_free()
 		return
-	if body.has_method("take_damage"):
-		body.take_damage(damage)
-	queue_free()
+	if body is TileMapLayer or body is TileMap or body is StaticBody2D:
+		queue_free()
